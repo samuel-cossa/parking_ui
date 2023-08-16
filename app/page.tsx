ad3c05ed53d68b5f68e119d3cc4126a8d696383f
samuel-cossa <ar.sam.cossa@gmail.com>
2023-08-16 17:04:20 +0200

    My first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+// import { Hero } from '@/components/Hero'
+import { Hero } from '@/components'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className='overflow-hidden'>
+      <Hero/>
+    </main>
+  )
+}
